e592f7a18e9124c4a032799b3da6598d9a45c1e6
Lee David <leedav87@gmail.com>
2021-08-23 15:01:51 -0300

    finalizando funcion ingresarReserva() y probanbo metodo getItem()

--- js/script.js
+++ js/script.js
@@ -10,12 +10,12 @@ function ingresarReserva() {
 
     do {
         reserva = prompt('que hotel? vacio para salir:');
-        if (item == '') {
+        if (reserva == '') {
             break;
         }
         cantidad = prompt('cuantas personas son?')
         if (cantidad && cantidad > 0) { //si ingreso algo (cantidad) Y ese ingreso es mayor a 0 (cantidad > 0)
-            localStorage.setItem(reserva, cantidad); //guardarlo en localStorage
+            localStorage.setItem(reserva.toUpperCase(), cantidad); //guardarlo en localStorage y todo los datos en MAYUSCULA
             cantidadReservas++;
         } else {
             ingresarReserva = false
@@ -27,3 +27,9 @@ function ingresarReserva() {
 }
 
 ingresarReserva();
+
+console.log(sessionStorage.getItem('cantidadReservas'));
+
+let cantidadReservas = localStorage.getItem('punta cana'.toUpperCase());
+
+console.log(` Cantidad de Reservas en Punta Cana es ${cantidadReservas}`);
